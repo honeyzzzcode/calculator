ea3dc30eee5cace43db15d0480c9ddea33126efc
honeyzzzcode <zlata200050@gmail.com>
2020-11-30 13:29:59 +0200

    added all possible expressions with 2 operations

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,7 +8,79 @@ package calculator;
 public class Calculator {
 
     public String calculate(String[] expr) {
-        return "0";
+        double a = Double.parseDouble(expr[0]);
+        String op1 = expr[1];
+        double b = Double.parseDouble(expr[2]);
+        String op2 = expr[3];
+        double c = Double.parseDouble(expr[4]);
+        String op = op1 + op2;
+        double result = 0;
+        switch (op1) {
+            case "+":
+                result = a + b ;
+                break;
+            case "-":
+                result = a - b ;
+                break;
+            case "/":
+                result = a / b ;
+                break;
+            case "*":
+                result = a * b ;
+                break;
+            default:
+                return "error";
+        }
+                switch (op) {
+
+                case "++":
+                    result = a + b + c;
+                    break;
+                case "+-":
+                    result = a + b - c;
+                    break;
+                case "-+":
+                    result = a - b + c;
+                    break;
+                case "--":
+                    result = a - b - c;
+                    break;
+                    case "/+":
+                        result = a / b + c;
+                        break;
+                    case "+/":
+                        result = a + (b / c);
+                        break;
+                    case "/-":
+                        result = a / b - c;
+                        break;
+                    case "-/":
+                        result = a - (b / c);
+                    case "//":
+                        result = a / b / c;
+                        break;
+                    case "-*":
+                        result = a - (b * c);
+                        break;
+                    case "*-":
+                        result = a * b - c;
+                        break;
+                    case "+*":
+                        result = a + (b * c);
+                        break;
+                    case "*+":
+                        result = a * b + c;
+                        break;
+                    case "**":
+                        result = a * b * c;
+                        break;
+
+
+                    default:
+                        return "error";
+            }
+        return String.valueOf(result);
+    }
     }
 
-}
+
